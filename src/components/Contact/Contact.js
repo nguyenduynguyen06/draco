e3c0f8122aa311d36c33f0c0af9a8ddf1b8eb316
nguyenduynguyen06 <20110530@student.hcmute.edu.vn>
2024-10-30 09:34:54 +0700

    update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -51,7 +51,7 @@ const Contact = () => {
           <span className="hihi">{t('contactPage.contactUs')}</span>
         </div>
         <div className="contact-us-cards">
-          <div className="contact-card">
+          <div className="contact-card"  onClick={showMap}>
             <EnvironmentOutlined className="icon-style" />
             <h3>{t('contactPage.cards.address.title')}</h3>
             <p>
@@ -60,6 +60,7 @@ const Contact = () => {
               {t('contactPage.cards.address.line3')}
             </p>
           </div>
+
           <div className="contact-card">
             <PhoneOutlined className="icon-style" />
             <h3>{t('contactPage.cards.phone.title')}</h3>
@@ -80,7 +81,7 @@ const Contact = () => {
         >
           <div className="map-container">
             <iframe
-              src="https://www.google.gg/maps/place/Ph%C6%B0%E1%BB%9Dng+7+Ph%C3%BA+Nhu%E1%BA%ADn,+Ph%C6%B0%E1%BB%9Dng+7,+Ph%C3%BA+Nhu%E1%BA%ADn,+H%E1%BB%93+Ch%C3%AD+Minh,+Vi%E1%BB%87t+Nam/@10.8028711,106.6889026,19z/data=!3m1!4b1!4m6!3m5!1s0x317528c5227901c9:0xec6f0dbb33f10779!8m2!3d10.8028711!4d106.6895463!16s%2Fg%2F11k3p1_w50?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.520714718376!2d106.83431057583897!3d10.847943257868891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317520bf41923763%3A0x9cadf5732a6c4b1a!2zMTM1IFBoxrDhu5tjIFRoaeG7h24sIExvbmcgVGjhuqFuaCBN4bu5LCBRdeG6rW4gOSwgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1730188882968!5m2!1svi!2s"
               width="100%"
               height="450"
               style={{ borderRadius: "15px", border: "1px solid #2D947A" }}
